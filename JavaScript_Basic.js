4f5aff0efa670293fdd33df6fac3d9b8a6bd6420
anmol-chhetri-G <anmolsinghchetri34@gmail.com>
2025-08-20 21:20:10 +0545

    Basic Overview 1

--- JavaScript_Basic.js
+++ JavaScript_Basic.js
@@ -1,2 +1,40 @@
-# run using node and make sure to install node
-# console.log("Beginning of JS basics");
+// ===== JavaScript Basics =====
+
+// run this using node and make sure to install node
+// Note: this is focused to learn in cli environment
+
+// === This is the basic Overview of the JavaScript ===
+
+// Variables: Store values that can change or stay constant
+
+// Use 'let' for variables that can change
+// let age = 25;
+
+// Use 'const' for constants that won’t change
+// const name = "Anmol";
+
+// Data types examples:
+// let isStudent = true;           // Boolean (true/false)
+// let score = 98.6;               // Number (integer or float)
+// let greeting = "Hello World!"; // String (text)
+
+// Functions: Reusable blocks of code
+// function sayHello(person) {
+//   return `Hello, ${person}!`;
+// }
+
+// Loops: Repeat actions multiple times
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// Conditionals: Make decisions based on conditions
+// let number = 10;
+// if (number > 5) {
+//   console.log("Number is greater than 5");
+// } else {
+//   console.log("Number is 5 or less");
+// }
+
+// To try the code, simply remove the '//' from the beginning of each line.
+
